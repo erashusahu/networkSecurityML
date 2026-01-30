e4e28bd6796fa54371e967dcad49b82e5d6845cd
Piyush2205 <sahuashutosh1408@gmail.com>
2026-01-30 16:06:46 +0530

    feat: Initialize a new modular machine learning project for network security, including core pipeline components, deployment configurations, and updated project metadata.

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,20 +1,22 @@
 import sys
 from networksecurity.logging import logger
 
-def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
-    return error_message
-
 class NetworkSecurityException(Exception):
-    def __init__(self, error_message, error_detail: sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
-
-    def __str__(self):  
-        return self.error_message
-
-
+    def __init__(self,error_message,error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+        
+        self.lineno=exc_tb.tb_lineno
+        self.file_name=exc_tb.tb_frame.f_code.co_filename 
+    
+    def __str__(self):
+        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        self.file_name, self.lineno, str(self.error_message))
+        
+if __name__=='__main__':
+    try:
+        logger.logging.info("Enter the try block")
+        a=1/0
+        print("This will not be printed",a)
+    except Exception as e:
+           raise NetworkSecurityException(e,sys)
--- setup.py
+++ setup.py
@@ -1,35 +1,39 @@
-from setuptools import find_packages, setup
+'''
+The setup.py file is an essential part of packaging and 
+distributing Python projects. It is used by setuptools 
+(or distutils in older Python versions) to define the configuration 
+of your project, such as its metadata, dependencies, and more
+'''
+
+from setuptools import find_packages,setup
 from typing import List
 
 def get_requirements()->List[str]:
     """
-    this function is return list of requirements 
+    Thiss function will return list of requirements
+    
     """
     requirement_lst:List[str]=[]
     try:
         with open('requirements.txt','r') as file:
-            #Read lines from thr file
+            #Read lines from the file
             lines=file.readlines()
-            #PRocess each line
+            ## Process each line
             for line in lines:
-                requirements=line.strip()
-                ## ignore empty file and -e .
-                if requirements and requirements!= '-e .':
-                    requirement_lst.append(requirements)
-
+                requirement=line.strip()
+                ## ignore empty lines and -e .
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
     except FileNotFoundError:
-        print("requirements.txt file no tfound") 
+        print("requirements.txt file not found")
 
-    return requirement_lst  
-
-
+    return requirement_lst
 
 setup(
-    name="NetWorkSecurity",
+    name="NetworkSecurity",
     version="0.0.1",
-    author="Ashutosh",
-    author_email="sahuashutosh1408@gmial.com",
+    author="Krish Naik",
+    author_email="krishnaik06@gmail.com",
     packages=find_packages(),
     install_requires=get_requirements()
-
 )
